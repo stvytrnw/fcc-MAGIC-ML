bd37f242db22738b1461ecf362a4c6dc98982d3c
L3RiZ <107563807+stvytrnw@users.noreply.github.com>
2023-10-01 21:37:04 +0200

    Update Mails.py

--- Mails.py
+++ Mails.py
@@ -10,11 +10,49 @@ from sklearn.linear_model import LogisticRegression
 
 df = pd.read_csv("emails.csv")
 
-for column in df:
-    plt.hist(df[df["class"] == 1][column], color='blue', label='Spam', alpha=0.7, density=True)
-    plt.hist(df[df["class"] == 0][column], color='red', label='Not Spam', alpha=0.7, density=True)
-    plt.title(column)
-    plt.xlabel(column)
-    plt.ylabel('Probability')
-    plt.legend()
-    plt.show()
+train, valid, test = np.split(df.sample(frac=1), [int(0.6*len(df)), int(0.8*len(df))])
+
+print(test)
+
+def scale_dataset(dataframe, oversample=False):
+    X = dataframe[dataframe.columns[1:-1]].values
+    y = dataframe[dataframe.columns[-1]].values
+    
+    scaler = StandardScaler()
+    X = scaler.fit_transform(X)
+    
+    if oversample:
+        ros = RandomOverSampler()
+        X , y = ros.fit_resample(X,y)
+    
+    data = np.hstack((X, np.reshape(y, (-1, 1))))
+    
+    return data, X, y
+
+train, X_train, y_train = scale_dataset(train, oversample=True)
+train, X_valid, y_valid = scale_dataset(valid, oversample=False)
+train, X_test, y_test = scale_dataset(test, oversample=False)
+
+# kNN 
+
+# knn_model = KNeighborsClassifier(n_neighbors=5)
+# knn_model.fit(X_train, y_train)
+# y_pred = knn_model.predict(X_test)
+
+# print(classification_report(y_test, y_pred))
+
+# Naive Bayes
+
+# nb_model = GaussianNB()
+# nb_model = nb_model.fit(X_train, y_train)
+
+# y_pred = nb_model.predict(X_test)
+# print(classification_report(y_test, y_pred))
+
+# Log Regression 
+
+log_model = LogisticRegression()
+log_model = log_model.fit(X_train, y_train)
+
+y_pred = log_model.predict(X_test)
+print(classification_report(y_test, y_pred))
