0c667563d30a7a02a0818037205e5656d5c348e8
L3RiZ <107563807+stvytrnw@users.noreply.github.com>
2023-10-16 21:01:16 +0200

    Update Mails.py

--- Mails.py
+++ Mails.py
@@ -7,25 +7,11 @@ from sklearn import svm
 from sklearn.model_selection import train_test_split, cross_val_score
 from sklearn.metrics import r2_score
 
-
 df = pd.read_csv("emails.csv")
 
 X = df[df.columns[1:-1]].values
 y = df[df.columns[-1]].values
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, shuffle=True)
-
-# def scale_dataset(X, y, oversample=False):
-#     scaler = StandardScaler()
-#     X = scaler.fit_transform(X)
-        
-#     if oversample:
-#         ros = RandomOverSampler()
-#         X , y = ros.fit_resample(X,y)
-    
-#     return X, y
-
-# X_train, y_train = scale_dataset(X_train, y_train, oversample=False)
-# X_test, y_test = scale_dataset(X_test, y_test, oversample=False)
 
 print('kNN')
 model = KNeighborsClassifier(n_neighbors=5)
