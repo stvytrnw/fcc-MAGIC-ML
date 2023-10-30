433a5e243a4886164c4ac2e4527e17db555cc0f6
L3RiZ <107563807+stvytrnw@users.noreply.github.com>
2023-10-30 15:38:26 +0100

    Update Mails.py

--- Mails.py
+++ Mails.py
@@ -67,51 +67,49 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 # print('Std score: {}'.format(scores.std()))
 # print()
 
-# print('NEURAL NETWORK')
-# model = MLPClassifier(random_state=42, max_iter=1000, hidden_layer_sizes=[3000,3000, 3000])
-# print('Train R2: {}'.format(r2_score(y_train, model.fit(X_train, y_train).predict(X_train))))
-# print('Test R2: {}'.format(r2_score(y_test, model.fit(X_train, y_train).predict(X_test))))
-# scores = cross_val_score(model, X_train, y_train, cv=5, scoring='r2')
-# print('Scores: {}'.format(scores))
-# print('Mean score: {}'.format(scores.mean()))
-# print('Std score: {}'.format(scores.std()))
-# print()
-
 print('NEURAL NETWORK')
-def plot_history(history):
-  fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(10, 4))
-  ax1.plot(history.history['loss'], label='loss')
-  ax1.plot(history.history['val_loss'], label='val_loss')
-  ax1.set_xlabel('Epoch')
-  ax1.set_ylabel('Binary crossentropy')
-  ax1.grid(True)
+model = MLPClassifier(random_state=42, max_iter=10000, hidden_layer_sizes=[100,100])
+print('Train R2: {}'.format(r2_score(y_train, model.fit(X_train, y_train).predict(X_train))))
+print('Test R2: {}'.format(r2_score(y_test, model.fit(X_train, y_train).predict(X_test))))
+scores = cross_val_score(model, X_train, y_train, cv=5, scoring='r2')
+print('Scores: {}'.format(scores))
+print('Mean score: {}'.format(scores.mean()))
+print('Std score: {}'.format(scores.std()))
+print()
 
-  ax2.plot(history.history['accuracy'], label='accuracy')
-  ax2.plot(history.history['val_accuracy'], label='val_accuracy')
-  ax2.set_xlabel('Epoch')
-  ax2.set_ylabel('Accuracy')
-  ax2.grid(True)
+# print('NEURAL NETWORK')
+# def plot_history(history):
+#   fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(10, 4))
+#   ax1.plot(history.history['loss'], label='loss')
+#   ax1.plot(history.history['val_loss'], label='val_loss')
+#   ax1.set_xlabel('Epoch')
+#   ax1.set_ylabel('Binary crossentropy')
+#   ax1.grid(True)
 
-  plt.show()
+#   ax2.plot(history.history['accuracy'], label='accuracy')
+#   ax2.plot(history.history['val_accuracy'], label='val_accuracy')
+#   ax2.set_xlabel('Epoch')
+#   ax2.set_ylabel('Accuracy')
+#   ax2.grid(True)
+
+#   plt.show()
   
-model = tf.keras.Sequential([
-    tf.keras.layers.Dense(3000, activation='relu', input_shape=(3000,)),
-    tf.keras.layers.Dropout(0.2),
-    tf.keras.layers.Dense(3000, activation='relu'),
-    tf.keras.layers.Dropout(0.2),
-    tf.keras.layers.Dense(3000, activation='relu'),
-    tf.keras.layers.Dropout(0.2),
-    tf.keras.layers.Dense(1, activation='sigmoid')
-])
+# model = tf.keras.Sequential([
+#     tf.keras.layers.Dense(100, activation='relu', input_shape=(3000,)),
+#     # tf.keras.layers.Dropout(0.2),
+#     tf.keras.layers.Dense(100, activation='relu'),
+#     # tf.keras.layers.Dropout(0.2),
+#     tf.keras.layers.Dense(1, activation='sigmoid')
+# ])
 
-model.summary()
+# model.summary()
 
-model.compile(optimizer=tf.keras.optimizers.Adam(0.005), loss='binary_crossentropy',
-                metrics=['accuracy'])
-history = model.fit(
-  X_train, y_train, epochs=100, batch_size=128, validation_split=0.2, verbose=0
-)
+# model.compile(optimizer=tf.keras.optimizers.Adam(0.005), loss='binary_crossentropy',
+#                 metrics=['accuracy'])
+# history = model.fit(
+#   X_train, y_train, epochs=100, batch_size=128, validation_split=0.2, verbose=0
+# )
 
-plot_history(history)
-accuracy = model.evaluate(X_test, y_test)[1]
-print('Accuracy:', accuracy)
+# plot_history(history)
+# accuracy = model.evaluate(X_test, y_test)[1]
+# print('Accuracy:', accuracy)
